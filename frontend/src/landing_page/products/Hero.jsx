c94d17910b5b89360f623b97c941345bb30da71e
Roohi Tabassum <roohbintemajid@gmail.com>
2026-07-07 19:49:52 +0530

    Add product page with TradeX,Grow,Learn,Hub,and Connect API sections

--- frontend/src/landing_page/products/Hero.jsx
+++ frontend/src/landing_page/products/Hero.jsx
@@ -1,7 +1,11 @@
 function Hero() {
     return (
-        <h1>Hero</h1>
-      );
+<div className="container text-center p-5 mb-5 border-bottom" style={{lineHeight:"3rem"}}>
+    <h1 className="p-2 ">Technology</h1>
+    <h4 className="text-muted">Sleek,modern and inuitive trading platforms.</h4>
+               <p className="mt-2">Checkout our<a href="" style={{textDecoration:"none"}}> investment offerings <i className="fa fa-long-arrow-right" aria-hidden="true"></i></a></p>
+    
+</div>      );
 }
 
 export default Hero;
--- frontend/src/landing_page/products/ProductsPage.jsx
+++ frontend/src/landing_page/products/ProductsPage.jsx
@@ -4,14 +4,60 @@ import RightSection from "./RightSection";
 import Universe from "./Universe";
 
 function ProductsPage() {
-    return ( 
-        <div>
-        <Hero/>
-        <LeftSection/>
-        <RightSection/>
-        <Universe/>
-        </div>
-     );
+  return (
+    <div>
+      <Hero />
+        
+      <LeftSection
+        imageUrl="images/tradeX.png"
+        productName="TradeX"
+        productDescription=" Our ultra-fast flagship trading platform with streaming market data,
+  advanced charts, an elegant UI, and more. Enjoy the TradeX experience
+  seamlessly on your Android and iOS devices."
+        tryDemo=""
+        learnMore=""
+        googlePlay=""
+        appStore=""
+      />
+        <RightSection imageUrl="images/console.png"
+        productName="Hub"
+        productDescription=" The central dashboard for your TradeX account. Gain insights into
+  your trades and investments with in-depth reports and visualisations."
+      
+        learnMore=""
+       />
+      <LeftSection
+        imageUrl="images/coin.png"
+        productName="Grow"
+        productDescription="Buy direct mutual funds online, commission-free, delivered directly to
+  your Demat account. Enjoy the investment experience on your Android
+  and iOS devices."
+        tryDemo=""
+        learnMore=""
+        googlePlay=""
+        appStore=""
+      />
+        <RightSection imageUrl="images/tradexconnect.png"
+        productName="TradeX Connect API"
+        productDescription="  Build powerful trading platforms and experiences with our super
+  simple HTTP/JSON APIs. If you are a startup, build your investment
+  app and showcase it to our clientbase."
+        learnMore="" />
+      <LeftSection
+        imageUrl="images/varsity.png"
+        productName="Learn"
+        productDescription="  An easy to grasp, collection of stock market lessons with in-depth
+  coverage and illustrations. Content is broken down into bite-size cards
+  to help you learn on the go."
+        tryDemo=""
+        learnMore=""
+        googlePlay=""
+        appStore=""
+      />
+   
+      <Universe />
+    </div>
+  );
 }
 
 export default ProductsPage;
