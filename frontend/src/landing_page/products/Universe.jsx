2290e3e281c25a71721619634fc2b6c9fe7d9cb7
Roohi Tabassum <roohbintemajid@gmail.com>
2026-07-07 20:02:19 +0530

    Add product page with TradeX,Grow,Learn,Hub,and Connect API sections and signup button

--- frontend/src/landing_page/products/Universe.jsx
+++ frontend/src/landing_page/products/Universe.jsx
@@ -4,7 +4,9 @@ function Universe() {
     return ( <div className="container text-center mt-5 p-2">
      <h1>The TradeX Ecosystem</h1><br />
 <p>Extend your trading and investment experience even further with our partner platforms</p>
-<img src="images/companylogos.png" style={{height:"auto",width:"70%"}}/>
+<img src="images/companylogos.png" style={{height:"auto",width:"70%"}}/> <br />
+                <button className='p-2 btn btn-primary fs-5 mb-5' style={{width:"20%",margin:"0 auto"}}>Signup now</button>
+
 </div>
      );
 }
